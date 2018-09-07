3918314674ae91cd44450b6427a9c2df6abfd1a8
Jonathan Duperrier <jonathan.duperrier@gmail.com>
2018-09-07 14:52:23 +0200

    dynamic form with select correct bug on opening modal service windows

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var graphSchemaApp = angular.module('graphSchemaApp', ['ui.router', 'ngFileSaver','ngSanitize', 'ui.bootstrap', 'angularModalService', 'ngLoad']);
+var graphSchemaApp = angular.module('graphSchemaApp', ['ui.router', 'ngFileSaver','ngSanitize', 'ui.bootstrap', 'angularModalService']);
 
 graphSchemaApp.config(function($stateProvider, $urlRouterProvider) {
     
