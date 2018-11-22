a19894354dfdd7151d145a5e5cf3088d129c32d1
jonathanduperrier <jonathan.duperrier@gmail.com>
2018-11-22 17:30:06 +0100

    manage exception if param is undefined in python script generator

--- controller.js
+++ controller.js
@@ -168,198 +168,202 @@ graphSchemaApp.controller('graphController', function($scope, $rootScope, $state
 						var json_pop_param = {celltype: "error"};
 					}
 					if(json_pop_param.celltype != "error"){
-						if(json_pop_param.celltype == "IF_curr_alpha"){
-							str_inst += "pop_"+ val.id +" = " +
-							"p.Population(" + json_pop_param.size + ", p.IF_curr_alpha(v_rest="+json_pop_param.param_v_rest +
-							" , cm="+json_pop_param.param_cm +
-							" , tau_m="+json_pop_param.param_tau_m +
-							" , tau_refrac="+json_pop_param.param_tau_refrac +
-							" , tau_syn_E="+json_pop_param.param_tau_syn_E +
-							" , tau_syn_I="+json_pop_param.param_tau_syn_I +
-							" , i_offset="+json_pop_param.param_i_offset +
-							" , v_reset="+json_pop_param.param_v_reset +
-							" , v_thresh="+json_pop_param.param_v_thresh +
-							" ))\n"+ 
-							"pop_"+ val.id +".initialize(v="+json_pop_param.init_v +
-							" , isyn_exc="+json_pop_param.init_isyn_exc +
-							" , isyn_inh="+json_pop_param.init_isyn_inh +
-							" , label="+json_pop_param.name_value +
-							" )\n";
-						}
-						if(json_pop_param.celltype == "IF_curr_exp"){
-							str_inst += "pop_"+ val.id +" = p.Population(" + json_pop_param.size + ", p.IF_curr_exp(v_rest="+json_pop_param.param_v_rest +
-							" , cm="+json_pop_param.param_cm +
-							" , tau_m="+json_pop_param.param_tau_m +
-							" , tau_refrac="+json_pop_param.param_tau_refrac +
-							" , tau_syn_E="+json_pop_param.param_tau_syn_E +
-							" , tau_syn_I="+json_pop_param.param_tau_syn_I +
-							" , i_offset="+json_pop_param.param_i_offset +
-							" , v_reset="+json_pop_param.param_v_reset +
-							" , v_thresh="+json_pop_param.param_v_thresh +
-							" ))\n"+
-							"pop_"+ val.id +".initialize(v="+json_pop_param.init_v +
-							" , isyn_exc="+json_pop_param.init_isyn_exc +
-							" , isyn_inh="+json_pop_param.init_isyn_inh +
-							" , label="+json_pop_param.name_value +
-							" )\n";
-						}
-						if(json_pop_param.celltype == "IF_cond_alpha"){
-							str_inst += "pop_"+ val.id +" = p.Population(" + json_pop_param.size + ", p.IF_cond_alpha(v_rest="+json_pop_param.param_v_rest +
-							" , cm="+json_pop_param.param_cm +
-							" , tau_m="+json_pop_param.param_tau_m +
-							" , tau_refrac="+json_pop_param.param_tau_refrac +
-							" , tau_syn_E="+json_pop_param.param_tau_syn_E +
-							" , tau_syn_I="+json_pop_param.param_tau_syn_I +
-							" , e_rev_E="+json_pop_param.param_e_rev_E +
-							" , e_rev_I="+json_pop_param.param_e_rev_I +
-							" , v_thresh="+json_pop_param.param_v_thresh +
-							" , v_reset="+json_pop_param.param_v_reset +
-							" , i_offset="+json_pop_param.param_i_offset +
-							" ))\n"+
-							"pop_"+ val.id +".initialize(v="+json_pop_param.init_v +
-							" , gsyn_exc="+json_pop_param.init_gsyn_exc +
-							" , gsyn_inh="+json_pop_param.init_gsyn_inh +
-							" , label="+json_pop_param.name_value +
-							" )\n";
-						}
-						if(json_pop_param.celltype == "IF_cond_exp"){
-							str_inst += "pop_"+ val.id +" = p.Population(" + json_pop_param.size + ", p.IF_cond_exp(v_rest="+json_pop_param.param_v_rest +
-							" , cm="+json_pop_param.param_cm +
-							" , tau_m="+json_pop_param.param_tau_m +
-							" , tau_refrac="+json_pop_param.param_tau_refrac +
-							" , tau_syn_E="+json_pop_param.param_tau_syn_E +
-							" , tau_syn_I="+json_pop_param.param_tau_syn_I +
-							" , e_rev_E="+json_pop_param.param_e_rev_E +
-							" , e_rev_I="+json_pop_param.param_e_rev_I +
-							" , v_thresh="+json_pop_param.param_v_thresh +
-							" , v_reset="+json_pop_param.param_v_reset +
-							" , i_offset="+json_pop_param.param_i_offset +
-							" ))\n"+
-							"pop_"+ val.id +".initialize(v="+json_pop_param.init_v +
-							" , gsyn_exc="+json_pop_param.init_gsyn_exc +
-							" , gsyn_inh="+json_pop_param.init_gsyn_inh +
-							" , label="+json_pop_param.name_value +
-							" )\n";
-						}
-						if(json_pop_param.celltype == "HH_cond_exp"){
-							str_inst += "pop_"+ val.id +" = p.Population(" + json_pop_param.size + ", p.HH_cond_exp(gbar_Na="+json_pop_param.param_gbar_Na +
-							" , gbar_K="+json_pop_param.param_gbar_K +
-							" , g_leak="+json_pop_param.param_g_leak +
-							" , cm="+json_pop_param.param_cm +
-							" , v_offset="+json_pop_param.param_v_offset +
-							" , e_rev_Na="+json_pop_param.param_e_rev_Na +
-							" , e_rev_K="+json_pop_param.param_e_rev_K +
-							" , e_rev_leak="+json_pop_param.param_e_rev_leak +
-							" , e_rev_E="+json_pop_param.param_e_rev_E +
-							" , e_rev_I="+json_pop_param.param_e_rev_I +
-							" , tau_syn_E="+json_pop_param.param_tau_syn_E +
-							" , tau_syn_I="+json_pop_param.param_tau_syn_I +
-							" , i_offset="+json_pop_param.param_i_offset +
-							" ))\n"+
-							"pop_"+ val.id +".initialize(v="+json_pop_param.init_v +
-							" , gsyn_exc="+json_pop_param.init_gsyn_exc +
-							" , gsyn_inh="+json_pop_param.init_gsyn_inh +
-							" , label="+json_pop_param.name_value +
-							" )\n";
-						}
-						if(json_pop_param.celltype == "EIF_cond_alpha_isfa_ista"){
-							str_inst += "pop_"+ val.id +" = p.Population(" + json_pop_param.size + ", p.EIF_cond_alpha_isfa_ista(cm="+json_pop_param.param_cm +
-							" , tau_refrac="+json_pop_param.param_tau_refrac +
-							" , v_spike="+json_pop_param.param_v_spike +
-							" , v_reset="+json_pop_param.param_v_reset +
-							" , v_rest="+json_pop_param.param_v_rest +
-							" , tau_m="+json_pop_param.param_tau_m +
-							" , i_offset="+json_pop_param.param_i_offset +
-							" , a="+json_pop_param.param_a +
-							" , b="+json_pop_param.param_b +
-							" , delta_T="+json_pop_param.param_delta_T +
-							" , tau_w="+json_pop_param.param_tau_w +
-							" , v_thresh="+json_pop_param.param_v_thresh +
-							" , e_rev_E="+json_pop_param.param_e_rev_E +
-							" , tau_syn_E="+json_pop_param.param_tau_syn_E +
-							" , e_rev_I="+json_pop_param.param_e_rev_I +
-							" , tau_syn_I="+json_pop_param.param_tau_syn_I +
-							" ))\n"+
-							"pop_"+ val.id +".initialize(v="+json_pop_param.init_v +
-							" , w="+json_pop_param.init_w +
-							" , gsyn_exc="+json_pop_param.init_gsyn_exc +
-							" , gsyn_inh="+json_pop_param.init_gsyn_inh +
-							" , label="+json_pop_param.name_value +
-							" )\n";
-						}
-						if(json_pop_param.celltype == "SpikeSourcePoisson"){
+						try {
+							if(json_pop_param.celltype == "IF_curr_alpha"){
+								str_inst += "pop_"+ val.id +" = " +
+								"p.Population(" + json_pop_param.size + ", p.IF_curr_alpha(v_rest="+json_pop_param.param_v_rest +
+								" , cm="+json_pop_param.param_cm +
+								" , tau_m="+json_pop_param.param_tau_m +
+								" , tau_refrac="+json_pop_param.param_tau_refrac +
+								" , tau_syn_E="+json_pop_param.param_tau_syn_E +
+								" , tau_syn_I="+json_pop_param.param_tau_syn_I +
+								" , i_offset="+json_pop_param.param_i_offset +
+								" , v_reset="+json_pop_param.param_v_reset +
+								" , v_thresh="+json_pop_param.param_v_thresh +
+								" ))\n"+ 
+								"pop_"+ val.id +".initialize(v="+json_pop_param.init_v +
+								" , isyn_exc="+json_pop_param.init_isyn_exc +
+								" , isyn_inh="+json_pop_param.init_isyn_inh +
+								" , label="+json_pop_param.name_value +
+								" )\n";
+							}
+							if(json_pop_param.celltype == "IF_curr_exp"){
+								str_inst += "pop_"+ val.id +" = p.Population(" + json_pop_param.size + ", p.IF_curr_exp(v_rest="+json_pop_param.param_v_rest +
+								" , cm="+json_pop_param.param_cm +
+								" , tau_m="+json_pop_param.param_tau_m +
+								" , tau_refrac="+json_pop_param.param_tau_refrac +
+								" , tau_syn_E="+json_pop_param.param_tau_syn_E +
+								" , tau_syn_I="+json_pop_param.param_tau_syn_I +
+								" , i_offset="+json_pop_param.param_i_offset +
+								" , v_reset="+json_pop_param.param_v_reset +
+								" , v_thresh="+json_pop_param.param_v_thresh +
+								" ))\n"+
+								"pop_"+ val.id +".initialize(v="+json_pop_param.init_v +
+								" , isyn_exc="+json_pop_param.init_isyn_exc +
+								" , isyn_inh="+json_pop_param.init_isyn_inh +
+								" , label="+json_pop_param.name_value +
+								" )\n";
+							}
+							if(json_pop_param.celltype == "IF_cond_alpha"){
+								str_inst += "pop_"+ val.id +" = p.Population(" + json_pop_param.size + ", p.IF_cond_alpha(v_rest="+json_pop_param.param_v_rest +
+								" , cm="+json_pop_param.param_cm +
+								" , tau_m="+json_pop_param.param_tau_m +
+								" , tau_refrac="+json_pop_param.param_tau_refrac +
+								" , tau_syn_E="+json_pop_param.param_tau_syn_E +
+								" , tau_syn_I="+json_pop_param.param_tau_syn_I +
+								" , e_rev_E="+json_pop_param.param_e_rev_E +
+								" , e_rev_I="+json_pop_param.param_e_rev_I +
+								" , v_thresh="+json_pop_param.param_v_thresh +
+								" , v_reset="+json_pop_param.param_v_reset +
+								" , i_offset="+json_pop_param.param_i_offset +
+								" ))\n"+
+								"pop_"+ val.id +".initialize(v="+json_pop_param.init_v +
+								" , gsyn_exc="+json_pop_param.init_gsyn_exc +
+								" , gsyn_inh="+json_pop_param.init_gsyn_inh +
+								" , label="+json_pop_param.name_value +
+								" )\n";
+							}
+							if(json_pop_param.celltype == "IF_cond_exp"){
+								str_inst += "pop_"+ val.id +" = p.Population(" + json_pop_param.size + ", p.IF_cond_exp(v_rest="+json_pop_param.param_v_rest +
+								" , cm="+json_pop_param.param_cm +
+								" , tau_m="+json_pop_param.param_tau_m +
+								" , tau_refrac="+json_pop_param.param_tau_refrac +
+								" , tau_syn_E="+json_pop_param.param_tau_syn_E +
+								" , tau_syn_I="+json_pop_param.param_tau_syn_I +
+								" , e_rev_E="+json_pop_param.param_e_rev_E +
+								" , e_rev_I="+json_pop_param.param_e_rev_I +
+								" , v_thresh="+json_pop_param.param_v_thresh +
+								" , v_reset="+json_pop_param.param_v_reset +
+								" , i_offset="+json_pop_param.param_i_offset +
+								" ))\n"+
+								"pop_"+ val.id +".initialize(v="+json_pop_param.init_v +
+								" , gsyn_exc="+json_pop_param.init_gsyn_exc +
+								" , gsyn_inh="+json_pop_param.init_gsyn_inh +
+								" , label="+json_pop_param.name_value +
+								" )\n";
+							}
+							if(json_pop_param.celltype == "HH_cond_exp"){
+								str_inst += "pop_"+ val.id +" = p.Population(" + json_pop_param.size + ", p.HH_cond_exp(gbar_Na="+json_pop_param.param_gbar_Na +
+								" , gbar_K="+json_pop_param.param_gbar_K +
+								" , g_leak="+json_pop_param.param_g_leak +
+								" , cm="+json_pop_param.param_cm +
+								" , v_offset="+json_pop_param.param_v_offset +
+								" , e_rev_Na="+json_pop_param.param_e_rev_Na +
+								" , e_rev_K="+json_pop_param.param_e_rev_K +
+								" , e_rev_leak="+json_pop_param.param_e_rev_leak +
+								" , e_rev_E="+json_pop_param.param_e_rev_E +
+								" , e_rev_I="+json_pop_param.param_e_rev_I +
+								" , tau_syn_E="+json_pop_param.param_tau_syn_E +
+								" , tau_syn_I="+json_pop_param.param_tau_syn_I +
+								" , i_offset="+json_pop_param.param_i_offset +
+								" ))\n"+
+								"pop_"+ val.id +".initialize(v="+json_pop_param.init_v +
+								" , gsyn_exc="+json_pop_param.init_gsyn_exc +
+								" , gsyn_inh="+json_pop_param.init_gsyn_inh +
+								" , label="+json_pop_param.name_value +
+								" )\n";
+							}
+							if(json_pop_param.celltype == "EIF_cond_alpha_isfa_ista"){
+								str_inst += "pop_"+ val.id +" = p.Population(" + json_pop_param.size + ", p.EIF_cond_alpha_isfa_ista(cm="+json_pop_param.param_cm +
+								" , tau_refrac="+json_pop_param.param_tau_refrac +
+								" , v_spike="+json_pop_param.param_v_spike +
+								" , v_reset="+json_pop_param.param_v_reset +
+								" , v_rest="+json_pop_param.param_v_rest +
+								" , tau_m="+json_pop_param.param_tau_m +
+								" , i_offset="+json_pop_param.param_i_offset +
+								" , a="+json_pop_param.param_a +
+								" , b="+json_pop_param.param_b +
+								" , delta_T="+json_pop_param.param_delta_T +
+								" , tau_w="+json_pop_param.param_tau_w +
+								" , v_thresh="+json_pop_param.param_v_thresh +
+								" , e_rev_E="+json_pop_param.param_e_rev_E +
+								" , tau_syn_E="+json_pop_param.param_tau_syn_E +
+								" , e_rev_I="+json_pop_param.param_e_rev_I +
+								" , tau_syn_I="+json_pop_param.param_tau_syn_I +
+								" ))\n"+
+								"pop_"+ val.id +".initialize(v="+json_pop_param.init_v +
+								" , w="+json_pop_param.init_w +
+								" , gsyn_exc="+json_pop_param.init_gsyn_exc +
+								" , gsyn_inh="+json_pop_param.init_gsyn_inh +
+								" , label="+json_pop_param.name_value +
+								" )\n";
+							}
+							if(json_pop_param.celltype == "SpikeSourcePoisson"){
+								str_inst += "";
+							}
+							if(json_pop_param.celltype == "SpikeSourceArray"){
+								str_inst += "";
+							}
+							if(json_pop_param.celltype == "projection"){
+								var synapse_type = json_pop_param.synapse_type
+								if(json_pop_param.connectors_type == "AllToAll"){
+									if(synapse_type == 'static'){
+										str_inst += "prj_"+ val.id +" = p.Projection(pop_"+val.source.id+", pop_"+val.target.id+", p.AllToAllConnector(), " +
+										"p.StaticSynapse())\n";
+									} else if(synapse_type == 'receptor_type'){
+										str_inst += "" +
+										" )\n";
+									}
+								}
+								if(json_pop_param.connectors_type == "OneToOne"){
+									if(synapse_type == 'static'){
+										str_inst += "prj_"+ val.id +" = p.Projection(pop_"+val.source.id+", pop_"+val.target.id+", p.OneToOneConnector()," +
+										"p.StaticSynapse())\n";
+									}
+								}
+								if(json_pop_param.connectors_type == "FixedProbability"){
+									if(synapse_type == 'static'){
+										str_inst += "prj_"+ val.id +" = p.Projection(pop_"+val.source.id+", pop_"+val.target.id+
+										", p.FixedProbabilityConnector(" + 
+										json_pop_param.FixedProbability_p_connect + ", " + 
+										json_pop_param.FixedProbability_allow_self_connections +
+										")," +
+										"p.StaticSynapse())\n";
+									}
+								}
+								if(json_pop_param.connectors_type == "FromFile"){
+									if(synapse_type == 'static'){
+										str_inst += "prj_"+ val.id +" = p.Projection(pop_"+val.source.id + ", pop_"+val.target.id + 
+										", p.FromFileConnector(" +
+										json_pop_param.FromFile_file + ", " + 
+										json_pop_param.FromFile_distributed + ", " + 
+										json_pop_param.FromFile_safe + ", " + 
+										json_pop_param.FromFile_callback + 
+										")," +
+										"p.StaticSynapse())\n";
+									}
+								}
+								if(json_pop_param.connectors_type == "FixedNumberPre"){
+									if(synapse_type == 'static'){
+										str_inst += "prj_"+ val.id +" = p.Projection(pop_"+val.source.id+", pop_"+val.target.id + 
+										", p.FixedNumberPreConnector(" + 
+										json_pop_param.FixedNumberPre_n + ", " +
+										json_pop_param.FixedNumberPre_with_replacement + ", " +
+										json_pop_param.FixedNumberPre_allow_self_connections +
+										")," +
+										"p.StaticSynapse())\n";
+									}
+								}
+								if(json_pop_param.connectors_type == "FixedNumberPost"){
+									if(synapse_type == 'static'){
+										str_inst += "prj_"+ val.id +" = p.Projection(pop_"+val.source.id+", pop_"+val.target.id + 
+										", p.FixedNumberPostConnector(" + 
+										json_pop_param.FixedNumberPost_n + ", " +
+										json_pop_param.FixedNumberPost_with_replacement + ", " +
+										json_pop_param.FixedNumberPost_allow_self_connections +
+										")," +
+										"p.StaticSynapse())\n";
+									}
+								}
+								if(json_pop_param.connectors_type == "FixedTotalNumber"){
+									if(synapse_type == 'static'){
+										str_inst += "prj_"+ val.id +" = p.Projection(pop_"+val.source.id+", pop_"+val.target.id+", p.FixedTotalNumberConnector()," +
+										"p.StaticSynapse())\n";
+									}
+								}
+							}
+						} catch(error) {
 							str_inst += "";
-						}
-						if(json_pop_param.celltype == "SpikeSourceArray"){
-							str_inst += "";
-						}
-						if(json_pop_param.celltype == "projection"){
-							var synapse_type = json_pop_param.synapse_type
-							if(json_pop_param.connectors_type == "AllToAll"){
-								if(synapse_type == 'static'){
-									str_inst += "prj_"+ val.id +" = p.Projection(pop_"+val.source.id+", pop_"+val.target.id+", p.AllToAllConnector(), " +
-									"p.StaticSynapse())\n";
-								} else if(synapse_type == 'receptor_type'){
-									str_inst += "" +
-									" )\n";
-								}
-							}
-							if(json_pop_param.connectors_type == "OneToOne"){
-								if(synapse_type == 'static'){
-									str_inst += "prj_"+ val.id +" = p.Projection(pop_"+val.source.id+", pop_"+val.target.id+", p.OneToOneConnector()," +
-									"p.StaticSynapse())\n";
-								}
-							}
-							if(json_pop_param.connectors_type == "FixedProbability"){
-								if(synapse_type == 'static'){
-									str_inst += "prj_"+ val.id +" = p.Projection(pop_"+val.source.id+", pop_"+val.target.id+
-									", p.FixedProbabilityConnector(" + 
-									json_pop_param.FixedProbability_p_connect + ", " + 
-									json_pop_param.FixedProbability_allow_self_connections +
-									")," +
-									"p.StaticSynapse())\n";
-								}
-							}
-							if(json_pop_param.connectors_type == "FromFile"){
-								if(synapse_type == 'static'){
-									str_inst += "prj_"+ val.id +" = p.Projection(pop_"+val.source.id + ", pop_"+val.target.id + 
-									", p.FromFileConnector(" +
-									json_pop_param.FromFile_file + ", " + 
-									json_pop_param.FromFile_distributed + ", " + 
-									json_pop_param.FromFile_safe + ", " + 
-									json_pop_param.FromFile_callback + 
-									")," +
-									"p.StaticSynapse())\n";
-								}
-							}
-							if(json_pop_param.connectors_type == "FixedNumberPre"){
-								if(synapse_type == 'static'){
-									str_inst += "prj_"+ val.id +" = p.Projection(pop_"+val.source.id+", pop_"+val.target.id + 
-									", p.FixedNumberPreConnector(" + 
-									json_pop_param.FixedNumberPre_n + ", " +
-									json_pop_param.FixedNumberPre_with_replacement + ", " +
-									json_pop_param.FixedNumberPre_allow_self_connections +
-									")," +
-									"p.StaticSynapse())\n";
-								}
-							}
-							if(json_pop_param.connectors_type == "FixedNumberPost"){
-								if(synapse_type == 'static'){
-									str_inst += "prj_"+ val.id +" = p.Projection(pop_"+val.source.id+", pop_"+val.target.id + 
-									", p.FixedNumberPostConnector(" + 
-									json_pop_param.FixedNumberPost_n + ", " +
-									json_pop_param.FixedNumberPost_with_replacement + ", " +
-									json_pop_param.FixedNumberPost_allow_self_connections +
-									")," +
-									"p.StaticSynapse())\n";
-								}
-							}
-							if(json_pop_param.connectors_type == "FixedTotalNumber"){
-								if(synapse_type == 'static'){
-									str_inst += "prj_"+ val.id +" = p.Projection(pop_"+val.source.id+", pop_"+val.target.id+", p.FixedTotalNumberConnector()," +
-									"p.StaticSynapse())\n";
-								}
-							}
 						}
 					}
 				}
